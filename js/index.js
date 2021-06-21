37c20fa42293e913d570bb16313d3aab4c257281
Louis Flores <louisdefleurs@gmail.com>
2021-06-21 16:48:20 -0400

    nav opacity cover

--- js/index.js
+++ js/index.js
@@ -1,32 +1,16 @@
-
-// Main page slide show
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 9000); // Change image every 2 seconds
-}
-
 // Navagation Dropdown menue
 
-
 // Navagation toggel menue
-     // Sidebar Navagation menue
-     function openNav() {
-      document.getElementById("mySidenav").style.width = "400px";
-      document.getElementById("home").style.width = "100%";
-      document.getElementById("home").style.height = "100%";
-      }
-      function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("home").style.width = "0";
-      document.getElementById("home").style.height = "0";
-      }
+// Sidebar Navagation menue
+
+const sideNavagation = document.getElementById("mySideNav");
+const sideNavagationOpacity = document.getElementById("sideNav-focus");
+
+function openNav() {
+  sideNavagation.style.width = "300px";
+  sideNavagationOpacity.style.opacity = 0.5;
+}
+function closeNav() {
+  sideNavagation.style.width = "0";
+  sideNavagationOpacity.style.opacity = 1;
+}
