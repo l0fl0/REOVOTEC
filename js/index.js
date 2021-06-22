986b1a6eee146d70fa609f414f6acd84bd40e6a7
Louis Flores <louisdefleurs@gmail.com>
2021-06-22 02:34:22 -0400

    combined homepage with about page

--- js/index.js
+++ js/index.js
@@ -14,3 +14,35 @@ function closeNav() {
   sideNavagation.style.width = "0";
   sideNavagationOpacity.style.opacity = 1;
 }
+
+//   History
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const year = document.getElementsByClassName("year-button");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.width = "0";
+  }
+  for (i = 0; i < year.length; i++) {
+    year[i].className = year[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.width = "100%";
+  year[slideIndex - 1].className += " active";
+}
